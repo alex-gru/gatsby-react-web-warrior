48c0d96a48e6cd12054ab4b6f2bcdeafbb37af25
Alexander Gruschina <gruschina@symptoma.com>
2021-11-18 00:51:56 +0100

    use query variables (slug for generating dynamic pages based on provided markdown files)

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,21 +1,46 @@
 import React from "react"
 import Layout from "../components/Layout"
-import { GatsbyImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import * as styles from "../styles/project-details.module.css"
+import { graphql } from "gatsby"
 
-function ProjectDetails(props) {
+function ProjectDetails({ data }) {
+  console.log(data)
+  const { html } = data.markdownRemark
+  const { title, stack, featuredImg } = data.markdownRemark.frontmatter
+  const image = getImage(featuredImg)
+  console.log(title, stack, image)
+
   return (
    <Layout>
      <div className={ styles.details }>
-       <h2>title</h2>
-       <h3>stack</h3>
+       <h2>{title}</h2>
+       <h3>{stack}</h3>
        <div className={ styles.featured }>
-         {/*<GatsbyImage alt={} image={} />*/}
+         <GatsbyImage alt="The featured image of the project" image={ image } />
        </div>
-       {/*<div className={ styles.html } dangerouslySetInnerHTML={} />*/}
+       <div className={ styles.html } dangerouslySetInnerHTML={ { __html: html }} />
      </div>
    </Layout>
   )
 }
 
 export default ProjectDetails
+export const query = graphql`
+  query ProjectDetails ($slug: String) {
+    markdownRemark(frontmatter: {slug: {eq: $slug}}) {
+      html
+      frontmatter {
+        title
+        stack
+        slug
+        date
+        featuredImg {
+          childImageSharp {
+              gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+          }
+        }
+      }
+    }
+  }
+`
