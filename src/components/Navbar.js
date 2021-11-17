c7f99fb4cf81f41a2c3a882bb91d4eca66f02864
Alexander Gruschina <gruschina@symptoma.com>
2021-11-17 22:12:13 +0100

    trigger a static query in the navbar component (GraphQL)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 function Navbar(props) {
+  const data = useStaticQuery(graphql`
+    query SiteInfo {
+      site {
+        siteMetadata {
+          copyright
+          description
+          title
+        }
+      }
+    }
+  `)
+  const { title } = data.site.siteMetadata
+
   return (
     <nav>
-      <h1>Web Warrior</h1>
+      <h1>{ title }</h1>
       <div className="links">
         <Link to={"/"}>Home</Link>
         <Link to={"/about"}>About</Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,10 @@
 import React from "react"
 import Layout from "../components/Layout"
 import * as styles from "../styles/home.module.css"
-import { graphql, Link } from "gatsby"
+import { Link } from "gatsby"
 
 export default function Home( { data }) {
   console.log(data)
-  const { title, description } = data.site.siteMetadata
-
   return (
     <Layout>
       <section className={styles.header}>
@@ -17,20 +15,7 @@ export default function Home( { data }) {
           <Link className={styles.btn} to={"/projects"}>My Portfolio Projects</Link>
         </div>
         <img src={"/image.jpg"} style={{ maxWidth: '100%' }}/>
-        <p>{ title } - { description }</p>
       </section>
     </Layout>
   )
 }
-
-export const query = graphql`
-query SiteInfo {
-  site {
-    siteMetadata {
-      copyright
-      description
-      title
-    }
-  }
-}
-`
