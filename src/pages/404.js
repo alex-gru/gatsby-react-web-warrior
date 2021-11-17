6113c08c544fc46e143c7618fd43a21330c9684a
Alexander Gruschina <gruschina@symptoma.com>
2021-11-17 20:59:26 +0100

    create basic pages and 404

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function
+
+NotFound(props) {
+  return (
+    <div>
+      <h2>404</h2>
+      <p>Sorry, not found.</p>
+    </div>
+  )
+}
+
+export default NotFound
---
+++ src/pages/about.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+function About(props) {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit
+        fringilla ligula, a mollis ligula. Cras dictum molestie lorem, id
+        laoreet neque feugiat vel. Pellentesque at elit tincidunt, elementum
+        quam eu, hendrerit enim. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit. Integer porta augue ut tellus facilisis molestie.
+        Suspendisse dignissim lacinia sapien et pulvinar. Nam malesuada cursus
+        sapien, a venenatis ex ullamcorper quis. Aliquam erat volutpat. Etiam
+        luctus, sapien vitae pretium pellentesque, sapien purus imperdiet lorem,
+        a condimentum urna augue ut ipsum. Maecenas viverra lectus vel nibh
+        placerat accumsan. Nam sodales venenatis nisl, id placerat risus
+        tincidunt eu. Cras ultricies lorem et rhoncus facilisis. Nulla ac sapien
+        nisi. Nam ligula est, placerat at laoreet ut, pretium sed justo. Aenean
+        scelerisque purus id tempor ultrices. Ut bibendum elit sit amet massa
+        consectetur, at suscipit leo pulvinar.
+      </p>
+      <p>
+        Nunc egestas mauris vitae lorem elementum, quis tincidunt nulla feugiat.
+        Maecenas non molestie mauris, sit amet ultrices ante. Vestibulum semper
+        at dui quis elementum. Praesent sed lobortis lorem. Integer id convallis
+        nulla. Donec sed lacus justo. Aliquam erat volutpat. Etiam ac tristique
+        nulla, id dignissim tellus. Fusce justo nibh, finibus vel tempor sit
+        amet, rutrum a diam. Proin a vestibulum lectus. Maecenas quis
+        consectetur est. Donec dapibus convallis enim. Phasellus mauris justo,
+        blandit sed purus eu, vulputate tempor felis. Aliquam posuere tempus
+        ultrices. Sed sit amet velit nibh.
+      </p>
+    </div>
+  )
+}
+
+export default About
